cfb62ed14249a03b465fb918e59d06a999326700
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-02-24 02:41:23 +0000

    saving

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,14 @@ class SessionsController < ApplicationController
 
     end
 
+    
+
     def create
         #How do we check
         @user = User.where({ email: params["email"] })[0]
         if @user
             if BCrypt::Password.new(@user.password) == params["password"]
+            session["user_id"] = @user.id
             flash[:notice] = "You logged in!"
              redirect_to "/companies"
             else
@@ -21,4 +24,11 @@ class SessionsController < ApplicationController
            
        end
     end
+
+    def destroy
+        session["user_id"] = nil
+        flash[:notice] = "Goodbye."
+        redirect_to "/sessions/new"
+    end
+
 end
